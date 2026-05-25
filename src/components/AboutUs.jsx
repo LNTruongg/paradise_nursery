86310ca48f5cb925be304af604c41d3db68370ca
Nhật Trường <lenhattruongbt71@gmail.com>
2026-05-25 10:49:43 +0700

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides beautiful indoor plants
+        for homes and offices.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
